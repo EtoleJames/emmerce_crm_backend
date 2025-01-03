3ab9bc4ed68cd0d681cb32922086322b28ce6e27
James Etole <etolejames1@gmail.com>
2025-01-03 09:11:55 +0300

    This commit implements the following:-   - Implements the authentication app.   - Intergrate Celery to the system.   - Add Docker configuration

--- emmerce_crm_backend/settings.py
+++ emmerce_crm_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from decouple import Config, RepositoryEnv
 
 # Load configuration from .env file
@@ -41,17 +42,21 @@ INSTALLED_APPS = [
     'drf_yasg',
     'corsheaders',
     'decouple',
+    'celery',
+    'django_celery_beat',
+    'django_celery_results',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'emmerce_crm_backend.apps.contacts',
-    'emmerce_crm_backend.apps.leads',
-    'emmerce_crm_backend.apps.notes',
-    'emmerce_crm_backend.apps.reminders',
+    
+    'emmerce_crm_backend.apps.authentication.apps.AuthenticationConfig',
+    'emmerce_crm_backend.apps.contacts.apps.ContactsConfig',
+    'emmerce_crm_backend.apps.leads.apps.LeadsConfig',
+    'emmerce_crm_backend.apps.notes.apps.NotesConfig',
+    'emmerce_crm_backend.apps.reminders.apps.RemindersConfig',
 ]
 
 MIDDLEWARE = [
@@ -85,6 +90,23 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'emmerce_crm_backend.wsgi.application'
+
+# set the celery config
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
+#Redis
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+    }
+}
 
 
 # Database
@@ -139,12 +161,19 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Tell Django about the custom `CustomUser` model we created. The string
+# `authentication.CustomUser` tells Django we are referring to the `CustomUser` model in
+# the `authentication` module. This module is registered above in a setting
+# called `INSTALLED_APPS`.
+AUTH_USER_MODEL = 'authentication.CustomUser'
+
 REST_FRAMEWORK = {
-    'EXCEPTION_HANDLER': 'emmerce_crm_backend.apps.core.exceptions.core_exception_handler',
+    'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'emmerce_crm_backend.apps.authentication.backends.JWTAuthentication',),
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.TemplateHTMLRenderer',
@@ -160,7 +189,15 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_files/')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_files/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
